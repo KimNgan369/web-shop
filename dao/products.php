f1f0a8386edb83a95490f637cbe245084f448bfb
KimNgan369 <tranthikimngan70179@gmail.com>
2025-04-07 11:59:55 +0700

    Initial commit

---
+++ dao/products.php
@@ -0,0 +1,106 @@
+<?php
+require_once 'pdo.php';
+
+// function hang_hoa_insert($ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet, $so_luot_xem, $ngay_nhap, $mo_ta){
+//     $sql = "INSERT INTO hang_hoa(ten_hh, don_gia, giam_gia, hinh, ma_loai, dac_biet, so_luot_xem, ngay_nhap, mo_ta) VALUES (?,?,?,?,?,?,?,?,?)";
+//     pdo_execute($sql, $ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet==1, $so_luot_xem, $ngay_nhap, $mo_ta);
+// }
+
+// function hang_hoa_update($ma_hh, $ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet, $so_luot_xem, $ngay_nhap, $mo_ta){
+//     $sql = "UPDATE hang_hoa SET ten_hh=?,don_gia=?,giam_gia=?,hinh=?,ma_loai=?,dac_biet=?,so_luot_xem=?,ngay_nhap=?,mo_ta=? WHERE ma_hh=?";
+//     pdo_execute($sql, $ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet==1, $so_luot_xem, $ngay_nhap, $mo_ta, $ma_hh);
+// }
+
+// function hang_hoa_delete($ma_hh){
+//     $sql = "DELETE FROM hang_hoa WHERE  ma_hh=?";
+//     if(is_array($ma_hh)){
+//         foreach ($ma_hh as $ma) {
+//             pdo_execute($sql, $ma);
+//         }
+//     }
+//     else{
+//         pdo_execute($sql, $ma_hh);
+//     }
+// }
+
+
+
+function get_danhmucsp() {
+    $sql = "SELECT * FROM categories";  // Truy vấn lấy các danh mục
+    return pdo_query($sql);  // Trả về kết quả từ truy vấn
+}
+
+
+function get_bestselling($limi) {
+    // Truy vấn sản phẩm bán chạy dựa trên tổng số lượng bán ra
+    $sql = "SELECT p.id, p.name, p.price, p.image, SUM(oi.quantity) AS total_sold
+            FROM products p
+            JOIN order_items oi ON p.id = oi.product_id
+            JOIN orders o ON oi.order_id = o.id
+            WHERE o.status = 'Delivered'  -- Chỉ tính các đơn hàng đã giao
+            GROUP BY p.id
+            ORDER BY total_sold DESC
+            LIMIT " . $limi;
+    
+    return pdo_query($sql);
+}
+
+
+
+// function hang_hoa_select_by_id($ma_hh){
+//     $sql = "SELECT * FROM hang_hoa WHERE ma_hh=?";
+//     return pdo_query_one($sql, $ma_hh);
+// }
+
+// function hang_hoa_exist($ma_hh){
+//     $sql = "SELECT count(*) FROM hang_hoa WHERE ma_hh=?";
+//     return pdo_query_value($sql, $ma_hh) > 0;
+// }
+
+// function hang_hoa_tang_so_luot_xem($ma_hh){
+//     $sql = "UPDATE hang_hoa SET so_luot_xem = so_luot_xem + 1 WHERE ma_hh=?";
+//     pdo_execute($sql, $ma_hh);
+// }
+
+// function hang_hoa_select_top10(){
+//     $sql = "SELECT * FROM hang_hoa WHERE so_luot_xem > 0 ORDER BY so_luot_xem DESC LIMIT 0, 10";
+//     return pdo_query($sql);
+// }
+
+// function hang_hoa_select_dac_biet(){
+//     $sql = "SELECT * FROM hang_hoa WHERE dac_biet=1";
+//     return pdo_query($sql);
+// }
+
+// function hang_hoa_select_by_loai($ma_loai){
+//     $sql = "SELECT * FROM hang_hoa WHERE ma_loai=?";
+//     return pdo_query($sql, $ma_loai);
+// }
+
+// function hang_hoa_select_keyword($keyword){
+//     $sql = "SELECT * FROM hang_hoa hh "
+//             . " JOIN loai lo ON lo.ma_loai=hh.ma_loai "
+//             . " WHERE ten_hh LIKE ? OR ten_loai LIKE ?";
+//     return pdo_query($sql, '%'.$keyword.'%', '%'.$keyword.'%');
+// }
+
+// function hang_hoa_select_page(){
+//     if(!isset($_SESSION['page_no'])){
+//         $_SESSION['page_no'] = 0;
+//     }
+//     if(!isset($_SESSION['page_count'])){
+//         $row_count = pdo_query_value("SELECT count(*) FROM hang_hoa");
+//         $_SESSION['page_count'] = ceil($row_count/10.0);
+//     }
+//     if(exist_param("page_no")){
+//         $_SESSION['page_no'] = $_REQUEST['page_no'];
+//     }
+//     if($_SESSION['page_no'] < 0){
+//         $_SESSION['page_no'] = $_SESSION['page_count'] - 1;
+//     }
+//     if($_SESSION['page_no'] >= $_SESSION['page_count']){
+//         $_SESSION['page_no'] = 0;
+//     }
+//     $sql = "SELECT * FROM hang_hoa ORDER BY ma_hh LIMIT ".$_SESSION['page_no'].", 10";
+//     return pdo_query($sql);
+// }
---
+++ view/header1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MYTH - Rings Collection</title>
+    <link rel="stylesheet" href="layout/css/shop1.css">
+    <!-- <link rel="stylesheet" href="layout/css/home.css">
+    <link rel="stylesheet" href="layout/css/shop.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="layout/img/LOGO.png" alt="MYTH" class="logo">
+            <span class="brand-name">MYTH</span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+        <button class="sign-up-btn">SIGN UP</button>
+    </header>
