0f5eb2dccdb033f817184d0ac781bb36f490a671
killpup70 <52300175@student.tdtu.edu.vn>
2025-04-09 19:40:53 +0700

    em test a

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+//Nhúng kết nối CSDL
+include "dao/pdo.php";
+include "dao/products.php";
+
+include "view/header.php";
+
+//data cho trang chu 
+$bestsell=get_bestselling(3);
+$danhmucsp=get_danhmucsp();
+
+
+if (!isset($_GET['pg'])) {
+    include "view/home.php";
+} else {
+    switch ($_GET['pg']) {
+        case 'shop':
+            $dssp=get_dssp(6);
+            include "view/shop.php";
+            break;
+        case 'gioithieu':
+            include "view/gioithieu.php";
+            break;
+        default:
+            include "view/home.php";
+            break;
+    }
+}
+
+include "view/footer.php";
+?>
+<!-- helo -->
