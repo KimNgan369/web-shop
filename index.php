3a7ff3270a33a8f562d4003e86cb8513b7a13007
KimNgan369 <tranthikimngan70179@gmail.com>
2025-05-06 22:52:46 +0700

    doi mk

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ session_start();
 include "dao/pdo.php";
 include "dao/products.php";
 include "dao/user.php";
-include "view/header.php";
 
-// Data cho trang chủ 
-
+// Data chung cho các trang
 $bestsell = get_bestselling(3);
 $danhmucsp = get_danhmucsp();
 
@@ -41,15 +39,6 @@ if (!isset($_GET['pg'])) {
             include "view/footer.php";
             break;
 
-
-
-        // case 'rings':
-        //     $dssp=get_dssp(6);
-        //     $spchitiet='';
-        //     $splienquan='';
-        //     include "view/rings.php";
-        //     break;
-
         case 'rings':
             $dssp = get_dssp(6);
             $spchitiet = '';
@@ -58,15 +47,6 @@ if (!isset($_GET['pg'])) {
             include "view/rings.php";
             include "view/footer.php";
             break;
-
-
-        case 'rings':
-            $dssp=get_dssp(6);
-            $spchitiet='';
-            $splienquan='';
-            include "view/rings.php";
-            break;
-
 
         case 'ring_detail':
             if (isset($_GET['id']) && $_GET['id'] > 0) {
@@ -81,42 +61,7 @@ if (!isset($_GET['pg'])) {
                 echo "<h2 class='text-center my-5'>Không tìm thấy sản phẩm.</h2>";
                 include "view/footer.php";
             }
-           
-            include "view/ring_detail.php";
             break;
-        
-        case 'shoppingcart':
-            session_start();
-            
-            if (isset($_POST['addToCart'])) {
-                if (!isset($_SESSION['cart'])) {
-                    $_SESSION['cart'] = [];
-                }
-                
-                $product_id = (int)$_POST['id'];
-                
-                
-                if (isset($_SESSION['cart'][$product_id])) {
-                    $_SESSION['cart'][$product_id]['quantity'] += (int)$_POST['quantity'];
-                } else {
-                    $_SESSION['cart'][$product_id] = [
-                        'id' => $product_id,
-                        'name' => $_POST['name'],
-                        'price' => (float)$_POST['price'],
-                        'quantity' => (int)$_POST['quantity'],
-                        'image' => $_POST['image']
-                    ];
-                }
-                
-                
-                // Chuyển hướng để tránh submit lại
-                header("Location: <?= $base_url ?>/shoppingcart.php");
-                exit();
-            }
-            
-            include "view/shoppingcart.php";
-            break;
-
 
         case 'adduser':
             if (isset($_POST["signup"]) && ($_POST["signup"])) {
@@ -233,20 +178,11 @@ if (!isset($_GET['pg'])) {
             }
             break;
 
-        case 'checkout':
-            include "view/checkout.php";
-            break;
-
-        case 'ordercomplete':
-            include "view/ordercomplete.php";
-            break;
-
-            
         default:
             include "view/header.php";
             include "view/home.php";
             include "view/footer.php";
             break;
-}
+    }
 }
 ?>
